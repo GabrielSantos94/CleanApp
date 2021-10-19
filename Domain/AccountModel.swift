eea03a17fb6970b8b30f80758cf5c493dd53626a
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-10-19 20:24:12 -0300

    add Domain layer, with AccountModel and AddAccount model

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Gabriel Santos on 19/10/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
---
+++ Domain/AddAccount.swift
@@ -0,0 +1,19 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by Gabriel Santos on 19/10/21.
+//
+
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
