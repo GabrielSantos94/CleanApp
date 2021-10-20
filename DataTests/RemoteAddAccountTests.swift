2360d3235213ece31f028ab64c504ca4ea5e19e4
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-10-19 21:07:00 -0300

    making the first unit test

---
+++ DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,52 @@
+//
+//  RemoteAddAccount.swift
+//  RemoteAddAccount
+//
+//  Created by Gabriel Santos on 19/10/21.
+//
+
+import XCTest
+
+class RemoteAddAccount {
+    
+    private let url: URL
+    private let httpClient: HttpClient
+    
+    init(url: URL, httpClient: HttpClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    func add() {
+        httpClient.post(url: url)
+    }
+}
+
+protocol HttpClient {
+    func post(url: URL)
+}
+
+class RemoteAddAccountTests: XCTestCase {
+
+    func test_() {
+        let url = URL(string: "www.google.com")!
+        let httpClientSpy = HttpClientSpy()
+        
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        sut.add()
+        
+        XCTAssertEqual(httpClientSpy.url, url)
+    }
+}
+
+extension RemoteAddAccountTests {
+    
+    class HttpClientSpy: HttpClient {
+        
+        var url: URL?
+        
+        func post(url: URL) {
+            self.url = url
+        }
+    }
+}
