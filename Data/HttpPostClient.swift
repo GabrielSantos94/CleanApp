d14c2a1ad4b660eb10e718a4dffd828f678f015d
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-10-25 21:46:58 -0300

    move production code to there own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Gabriel Santos on 25/10/21.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
---
+++ Data/RemoteAddAccount.swift
@@ -0,0 +1,24 @@
+//
+//  Data.swift
+//  Data
+//
+//  Created by Gabriel Santos on 19/10/21.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteAddAccount {
+    
+    public let url: URL
+    public let httpClient: HttpPostClient
+    
+    public init(url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    public func add(addAccountModel: AddAccountModel) {
+        httpClient.post(to: url, with: addAccountModel.toData())
+    }
+}
