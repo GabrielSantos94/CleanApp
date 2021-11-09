44ea42865dfce2ce0aff3c8a33da63ee3d51612b
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-11-09 19:14:49 -0300

    adding infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Gabriel Santos on 09/11/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() throws {
+        
+    }
+}
