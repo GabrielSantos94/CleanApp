aa5244ac45db2603ad188e4822e60de505867885
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-11-09 20:52:02 -0300

    move duplicated code to a helper method to improve readability

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -33,32 +33,18 @@ class AlamofireAdapterTests: XCTestCase {
 
     func test_post_should_make_request_with_valid_url_and_method() throws {
         let url = makeURL()
-        let sut = makeSut()
         
-        sut.post(to: url, with: makeValidData())
-        
-        let exp = expectation(description: "waiting")
-        
-        URLProtocolStub.observeRequest { request in
+        testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
     
     func test_post_should_make_request_with_no_data() throws {
-        let sut = makeSut()
-        sut.post(to: makeURL(), with: nil)
-        
-        let exp = expectation(description: "waiting")
-        
-        URLProtocolStub.observeRequest { request in
+        testRequestFor(data: nil) { request in
             XCTAssertNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -69,6 +55,18 @@ extension AlamofireAdapterTests {
         configuration.protocolClasses = [URLProtocolStub.self]
         let session = Session(configuration: configuration)
         return AlamofireAdapter(session: session)
+    }
+    
+    func testRequestFor(url: URL = makeURL(), data: Data?, action: @escaping (URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.post(to: url, with: data)
+        let exp = expectation(description: "waiting")
+        
+        URLProtocolStub.observeRequest { request in
+            action(request)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
