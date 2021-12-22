eff8ecc7a71402d108582252cd230de61dc19281
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-12-22 16:44:36 -0300

    move UI+extension to a separate file

---
+++ UITests/Helpers/Test+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Test+Extensions.swift
+//  UITests
+//
+//  Created by Gabriel Santos on 22/12/21.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
