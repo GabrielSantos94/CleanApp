fae8249ddde1b06b349c7cf648798b1e475943a6
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-12-22 13:58:40 -0300

    move signupViewModel to its own file

---
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewModel.swift
+//  Presentation
+//
+//  Created by Gabriel Santos on 22/12/21.
+//
+
+import Foundation
+
+public struct SignUpViewModel {
+    public var name: String?
+    public var email: String?
+    public var password: String?
+    public var passwordConfirmation: String?
+    
+    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String? = nil) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
+}
