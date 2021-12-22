456a08f85ea4e38cbfd64d1897bb8aff7ba8a1ef
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-12-22 18:41:16 -0300

    add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Gabriel Santos on 22/12/21.
+//
+
+import XCTest
+//@testable import Validation
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test()  {
+    }
+}
