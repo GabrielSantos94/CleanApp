dbe7318cb69ba4672bbfca8f0fd9d3f7ae165e45
Gabriel Jesus Santos <santos.gabrieljesus@outlook.com>
2021-12-23 15:42:08 -0300

    add environment variables

---
+++ Main/Env/Enviroment.swift
@@ -0,0 +1,19 @@
+//
+//  Enviroment.swift
+//  Main
+//
+//  Created by Gabriel Santos on 23/12/21.
+//
+
+import Foundation
+
+public final class Enviroment {
+    
+    public enum Enviroments: String {
+        case apiBaseURL = "API_BASE_URL"
+    }
+    
+    public static func variable(key: Enviroments) -> String {
+        return Bundle.main.infoDictionary?[key.rawValue] as! String
+    }
+}
