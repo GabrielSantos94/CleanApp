7553024aeb3ef1f24ef96a129fe8853ea3fce404
GabrielSantos94 <santos.gabrieljesus@outlook.com>
2022-02-02 20:43:56 -0300

    add authentication usecases

---
+++ Domain/Use Cases/Authentication.swift
@@ -0,0 +1,24 @@
+//
+//  Authentication.swift
+//  Domain
+//
+//  Created by Gabriel Santos on 19/10/21.
+//
+
+import Foundation
+
+public protocol Authentication {
+    typealias AddAccountResult = Swift.Result<AccountModel, DomainError>
+    
+    func add(authenticationModel: AuthenticationModel, completion: @escaping (AddAccountResult) -> Void)
+}
+
+public struct AuthenticationModel: Model {
+    public var email: String
+    public var password: String
+    
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
