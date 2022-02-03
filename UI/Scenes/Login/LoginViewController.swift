043f0eeccc651fa00357560b149aa739dcd7bea0
GabrielSantos94 <santos.gabrieljesus@outlook.com>
2022-02-03 00:12:37 -0300

    adding loadingVC and its tests

--- UI/Scenes/Login/LoginViewController.swift
+++ UI/Scenes/Login/LoginViewController.swift
@@ -11,10 +11,12 @@ import Presentation
 
 public final class LoginViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var emailTextField: RoundedTextfield!
+    @IBOutlet weak var passwordTextField: RoundedTextfield!
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var loginButton: UIButton!
     
-    public var signUp: ((SignUpViewModel) -> Void)?
+    public var login: ((LoginViewModel) -> Void)?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,38 @@ public final class LoginViewController: UIViewController, Storyboarded {
     private func configure() {
         title = "4Dev"
         loginButton.layer.cornerRadius = 5
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         hideKeyboardOnTap()
+    }
+    
+    @objc private func loginButtonTapped() {
+        let viewModel = LoginViewModel(
+            email: emailTextField.text,
+            password: passwordTextField.text
+        )
+        
+        login?(viewModel)
+    }
+}
+
+
+extension LoginViewController: LoadingView {
+    
+    public func display(viewModel: LoadingViewModel) {
+        if viewModel.isLoading {
+            view.isUserInteractionEnabled = false
+            loadingIndicator.startAnimating()
+        } else {
+            view.isUserInteractionEnabled = true
+            loadingIndicator.stopAnimating()
+        }
+    }
+}
+
+extension LoginViewController: AlertView {
+    public func showMessage(viewModel: AlertViewModel) {
+        let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
     }
 }
--- UITests/Scenes/Login/LoginViewControllerTests.swift
+++ UITests/Scenes/Login/LoginViewControllerTests.swift
@@ -16,12 +16,33 @@ class LoginViewControllerTests: XCTestCase {
     func test_loading_is_hidden_at_start() {
         XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
     }
+    
+    func test_sut_implements_loadingView() {
+        XCTAssertNotNil(makeSut() as LoadingView)
+    }
+    
+    func test_sut_implements_alertView() {
+        XCTAssertNotNil(makeSut() as AlertView)
+    }
+    
+    func test_login_button_calls_login_on_tap() {
+        var loginViewModel: LoginViewModel?
+        let loginViewModelSpy: (LoginViewModel) -> Void = { loginViewModel = $0 }
+        let sut = makeSut(loginSpy: loginViewModelSpy)
+        
+        sut.loginButton?.simulateTap()
+        let email = sut.emailTextField?.text
+        let password = sut.passwordTextField?.text
+        
+        XCTAssertEqual(loginViewModel, LoginViewModel(email: email, password: password))
+    }
 }
 
 extension LoginViewControllerTests {
     
-    func makeSut(signUpSpy: ((SignUpViewModel) -> Void)? = nil) -> LoginViewController {
+    func makeSut(loginSpy: ((LoginViewModel) -> Void)? = nil) -> LoginViewController {
         let sut = LoginViewController.instantiate()
+        sut.login = loginSpy
         sut.loadViewIfNeeded()
         return sut
     }
