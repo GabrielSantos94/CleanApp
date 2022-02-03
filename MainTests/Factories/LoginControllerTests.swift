352d9a1a0047f362f8d56a6fcc0314ea70c54da5
GabrielSantos94 <santos.gabrieljesus@outlook.com>
2022-02-03 00:42:02 -0300

    more tests

--- MainTests/Factories/LoginControllerTests.swift
+++ MainTests/Factories/LoginControllerTests.swift
@@ -14,22 +14,23 @@ import UI
 
 class LoginControllerTests: XCTestCase {
 
-//    func test_background_request_should_complete_on_main_thread() {
-//        let addAccountSpy = AddAccountSpy()
-//        let sut = makeSignupController(with: MainQueueDispatchDecorator(addAccountSpy))
-//        sut.loadViewIfNeeded()
-//        sut.signUp?(makeSignUpViewModel())
-//
-//        let exp = expectation(description: "waiting")
-//        DispatchQueue.global().async {
-//            addAccountSpy.completeWithError(.unexpected)
-//            exp.fulfill()
-//        }
-//
-//        wait(for: [exp], timeout: 1)
-//
-//        checkMemoryLeak(for: sut)
-//    }
+    func test_background_request_should_complete_on_main_thread() {
+        let authenticationSpy = AuthenticationSpy()
+        let sut = makeLoginController(authentication: MainQueueDispatchDecorator(authenticationSpy))
+        
+        sut.loadViewIfNeeded()
+        sut.login?(makeLoginViewModel())
+
+        let exp = expectation(description: "waiting")
+        DispatchQueue.global().async {
+            authenticationSpy.completeWithError(.unexpected)
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+
+        checkMemoryLeak(for: sut)
+    }
     
     func test_signUp_compose_with_correct_validations() {
         let validations = makeLoginValidations()
