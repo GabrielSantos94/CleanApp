6d2646afc50835ac94c21d5c493d776945fb73e9
GabrielSantos94 <santos.gabrieljesus@outlook.com>
2022-02-08 20:50:07 -0300

    ensure welcome router calls navigation with signupFactory when called

--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -14,14 +14,23 @@ public final class WelcomeRouter {
     
     private let navigationController: NavigationController
     private let loginFactory: () -> LoginViewController
+    private let signUpFactory: () -> SignUpViewController
     
-    public init(navigationController: NavigationController, loginFactory: @escaping () -> LoginViewController) {
+    public init(navigationController: NavigationController,
+                loginFactory: @escaping () -> LoginViewController,
+                signUpFactory: @escaping () -> SignUpViewController) {
+        
         self.navigationController = navigationController
         self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
     }
     
     public func goToLogin() {
         navigationController.pushViewController(loginFactory())
+    }
+    
+    public func goToSignUp() {
+        navigationController.pushViewController(signUpFactory())
     }
 }
 
@@ -36,6 +45,16 @@ class WelcomeRouterTests: XCTestCase {
         XCTAssertEqual(navigationController.viewControllers.count, 1)
         XCTAssertTrue(navigationController.viewControllers.first is LoginViewController)
     }
+    
+    func testGoToSignUpCallsNavWithCorrectVC() {
+        
+        let (sut, navigationController) = makeSUT()
+        
+        sut.goToSignUp()
+        
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+        XCTAssertTrue(navigationController.viewControllers.first is SignUpViewController)
+    }
 }
 
 extension WelcomeRouterTests {
@@ -44,10 +63,12 @@ extension WelcomeRouterTests {
         
         let navigationController = NavigationController()
         let loginFactorySpy = LoginFactorySpy()
+        let signUpFactorySpy = SignUpFactorySpy()
         
         let sut = WelcomeRouter(
             navigationController: navigationController,
-            loginFactory: loginFactorySpy.makeLogin
+            loginFactory: loginFactorySpy.makeLogin,
+            signUpFactory: signUpFactorySpy.makeSignUp
         )
         
         return (sut, navigationController)
@@ -59,6 +80,13 @@ extension WelcomeRouterTests {
         
         func makeLogin() -> LoginViewController {
             return LoginViewController.instantiate()
+        }
+    }
+    
+    class SignUpFactorySpy {
+        
+        func makeSignUp() -> SignUpViewController {
+            return SignUpViewController.instantiate()
         }
     }
 }
